54413155270351d4519a1d56e7058a0384ef4dcf
Nolen Johnson <johnsonnolen@gmail.com>
2024-02-18 19:58:19 -0500

    berlna: Import bluetooth firmware

--- Android.mk
+++ Android.mk
@@ -6,4 +6,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),berlna)
 
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,b3cf3ea0ac7cff839bf5a32adcb38a9b0fdd0587)
+$(call add-radio-file-sha1-checked,radio/dsp.img,72c78cc1886db44fdb7893f8734a002552151a50)
+
 endif
