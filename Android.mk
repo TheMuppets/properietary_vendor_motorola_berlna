de72cd4c934d5b2c5420b2be6c6bf0e67dadc4cc
Nolen Johnson <johnsonnolen@gmail.com>
2024-08-10 20:58:56 -0400

    berlna: berlna_global-user 13 T1RMS33.1-110-17-12 f7bcd-35c05 release-keys

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),berlna)
 
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,55d7a6861eece93844a66bcfd49278192f910885)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,f03693c9c493e89587f564a22d374ca8caf20559)
 $(call add-radio-file-sha1-checked,radio/dsp.img,72c78cc1886db44fdb7893f8734a002552151a50)
 
 endif
